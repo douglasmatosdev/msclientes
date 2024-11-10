abb7bd1fff10db43392b2884222fba9cff0a0dcf
douglasmatosdev <douglasmatosdev@gmail.com>
2024-11-10 11:35:27 -0300

    add .zip resource

---
+++ src/main/java/com/douglasmatosdev/mscclientes/MscclientesApplication.java
@@ -0,0 +1,13 @@
+package com.douglasmatosdev.mscclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscclientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglasmatosdev/mscclientes/MscclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasmatosdev.mscclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
